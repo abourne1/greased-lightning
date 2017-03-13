2dc71dff16955bd66a48ad2472a7ee5615522d47
Andrew Bourne <andrew.bourne@uchicago.edu>
2017-03-13 08:02:40 +0000

    Changed hard link to soft link

--- control_servo.ino
+++ control_servo.ino
@@ -1,1 +1,19 @@
-/home/pi/sketchbook/control_servo/control_servo.ino
+#include<Servo.h>
+
+Servo servo;
+
+void setup() {
+  Serial.begin(14400);
+  servo.attach(9);
+  servo.write(90);
+}
+
+void loop() {
+  delay(100);
+  int numDigits = Serial.available();
+  if (numDigits > 0) {
+    int serialData = Serial.parseInt();
+    Serial.println(serialData);
+    servo.write(serialData);
+  }
+}
